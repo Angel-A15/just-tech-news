8728ca3dd656ace6cea3687dc893dd1ee718311d
Angel-A15 <106582411+Angel-A15@users.noreply.github.com>
2022-10-11 08:29:46 -0500

    added vote route 13.4

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
+const sequelize = require('../../config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -13,6 +14,6 @@ app.use(routes);
 
 // turn on connection to db and server
 //was supposed to restart express.js server after changing to true, but got error, will need to do
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
