fc31242b537e49a64683cca6b4257fdd2dff3ed9
Angel-A15 <106582411+Angel-A15@users.noreply.github.com>
2023-07-24 02:42:02 -0500

    added comment module to user routes find one

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -8,6 +8,7 @@ router.get('/', (req, res) => {
   console.log('======================');
   
   Post.findAll({
+
     attributes: [
       'id',
       'post_url',
@@ -20,7 +21,13 @@ router.get('/', (req, res) => {
     include: [
       {
         model: Comment,
-        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        attributes: [
+        'id', 
+        'comment_text',
+        'post_id',
+        'user_id',
+        'created_at'],
+
         include: {
           model: User,
           attributes: ['username']
